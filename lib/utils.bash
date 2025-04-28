fdbed9f29cad2c017909622990504d9acc34dbcf
Ivan Kerin <ikerin@gmail.com>
2025-04-28 12:15:19 +0000

    Fix download

--- lib/utils.bash
+++ lib/utils.bash
@@ -74,7 +74,7 @@ download_release() {
 		;;
 	esac
 
-	url="$GH_REPO/releases/download/${version}/${TOOL_NAME}_${version}_${release_file}.tar.gz"
+	url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}_${version}_${release_file}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
